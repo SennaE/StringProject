fd7e104c03c2f9e3a8a481490c4a9c015066725b
Senna <senna@win7sp1_vm>
2018-08-03 10:30:09 +0530

    FirstCommit with all files

---
+++ StringTest/src/com/testing/com/classOne.java
@@ -0,0 +1,16 @@
+package com.testing.com;
+
+public class classOne {
+
+	public static final String a = null;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String a ="abc";
+		System.out.println("main classOne");
+		
+	}
+
+}
