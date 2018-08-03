58ee8a18e76bec47feef5b467b0ff6313bc08961
win7sp1 <win7sp1@win7sp1_vm>
2018-08-03 10:31:24 +0530

    SecondClass Added

---
+++ StringTest/src/com/testing/com/classTwo.java
@@ -0,0 +1,12 @@
+package com.testing.com;
+
+public class classTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String b = "abc";
+		classOne c1 = new classOne();
+		System.out.println(c1.a == b);
+	}
+
+}
